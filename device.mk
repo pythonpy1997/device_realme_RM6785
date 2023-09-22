5cc7a7b3cf591811f1f4f0bd37738c76108992e7
pythonpy1997 <89606297+pythonpy1997@users.noreply.github.com>
2023-09-22 06:14:16 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -183,7 +183,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay-voltage
 
 PRODUCT_PACKAGES += \
     FrameworksOverlayRMX2151 \
